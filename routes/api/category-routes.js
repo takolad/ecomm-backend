7e33856d2237c2e383e0f00edd73484a42fe02da
John Hinojosa <hinojohn@ymail.com>
2021-04-21 15:42:22 -0500

    imported sequelize connection, added sequelize sync code to server.js, altered category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,17 +3,21 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   // find all categories
   // be sure to include its associated Products
+    const categoryData = await Category.findAll();
+    res.json(categoryData);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+    const categoryData = await Category.findByPk(req.params.id);
+    res.json(categoryData);
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
 });
 
