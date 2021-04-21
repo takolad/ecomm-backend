68e2bcc2ae41af6868769f59556377d0ff9440f4
John Hinojosa <hinojohn@ymail.com>
2021-04-21 16:56:46 -0500

    Fixed column definitions in ProductTag model, added try/catch statements to category routes

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -8,12 +8,14 @@ ProductTag.init(
   {
     // define columns
     product_id: {
+      type: DataTypes.INTEGER,
       references: {
         model: 'product',
         key: 'id',
       },
     },
     tag_id: {
+      type: DataTypes.INTEGER,
       references: {
         model: 'tag',
         key: 'id',
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,19 +6,35 @@ const { Category, Product } = require('../../models');
 router.get('/', async(req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
     const categoryData = await Category.findAll();
     res.json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.get('/:id', async(req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
     const categoryData = await Category.findByPk(req.params.id);
     res.json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create({
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
